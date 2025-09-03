ee202349c7e26a0eca829b086ea0760bd1f251be
Manuela Ramos <mramoso@eafit.edu.co>
2025-09-03 17:02:12 -0500

    Update multiprocessing_.py

--- Mineria/concurrent-downloads/multiprocessing_.py
+++ Mineria/concurrent-downloads/multiprocessing_.py
@@ -6,12 +6,12 @@ import utils
 import atexit
 
 # Variables compartidas entre procesos
-manager = multiprocessing.Manager() # Permite compartir listas y contadores entre procesos
+#manager = multiprocessing.Manager() # Permite compartir listas y contadores entre procesos
 # Contadores globales de éxito y fallo
-success_count = manager.Value('i', 0)
-fail_count = manager.Value('i', 0)
+#success_count = manager.Value('i', 0)
+#fail_count = manager.Value('i', 0)
 # El log se escribirá directamente en el archivo por cada proceso
-log_file_path = pathlib.Path("debugs") / "multiprocessing_download_log.txt"
+#log_file_path = pathlib.Path("debugs") / "multiprocessing_download_log.txt"
 
 session: requests.Session  # Se inicializa por proceso
 
@@ -23,11 +23,11 @@ def init_process():
     atexit.register(session.close) # Se asegura de finalizar la sesión cuando el proceso termina
 
 
-def download_one(row_and_output):
+def download_one(row, output_dir):
     """Descargar un Pokémon usando la sesión global del proceso."""
     #if session is None:
 
-    row, output_dir = row_and_output
+    #row, output_dir = row_and_output
     url = row["Sprite"]
     name = row["Pokemon"]
     type1 = row["Type1"]
@@ -40,38 +40,41 @@ def download_one(row_and_output):
             if response.status_code == 200:
                 # Guarda el contenido binario en el archivo
                 utils.write_binary(file_path, response.content)
-                with success_count.get_lock():
-                    success_count.value += 1
-                log_line = f"[OK] {name} ({type1})"
+                print(f"[OK] {name} ({type1})")
+                #with success_count.get_lock():
+                 #   success_count.value += 1
+                #log_line = f"[OK] {name} ({type1})"
             else:
-                with fail_count.get_lock():
-                    fail_count.value += 1
-                log_line = f"[FAIL] {name} ({type1}) - No content"
+                #with fail_count.get_lock():
+                 #   fail_count.value += 1
+                #log_line = f"[FAIL] {name} ({type1}) - No content"
+                print(f"[FAIL] {name} ({type1}) - No content")
     except Exception as e:
-        with fail_count.get_lock():
-            fail_count.value += 1
-        log_line = f"[ERROR] {name} ({type1}) - {e}"
+        print(f"[ERROR] {name} ({type1}) - {e}")
+        #with fail_count.get_lock():
+         #   fail_count.value += 1
+        #log_line = f"[ERROR] {name} ({type1}) - {e}"
     # Escribir el log en modo append
-    with open(log_file_path, "a") as f:
-        f.write(log_line + "\n")
+    #with open(log_file_path, "a") as f:
+    #   f.write(log_line + "\n")
 
 
 def main(output_dir: str, input_dir: str):
     output_path = pathlib.Path(output_dir)
     utils.ensure_dir(output_path)
 
-    debug_path = pathlib.Path("debugs")
-    utils.ensure_dir(debug_path)
-    log_file = log_file_path
+    #debug_path = pathlib.Path("debugs")
+    #utils.ensure_dir(debug_path)
+    #log_file = log_file_path
     # Limpiar el archivo de log antes de iniciar la corrida
-    open(log_file, "w").close()
+    #open(log_file, "w").close()
 
     # Leer todos los CSV
     input_path = pathlib.Path(input_dir)
     csv_files = list(input_path.glob("*.csv"))
     rows = list(utils.read_pokemons([str(f) for f in csv_files]))
 
-    start_time = time.perf_counter()
+    #start_time = time.perf_counter()
 
     # Preparar lista de tuplas (row, output_dir) para pasar a los procesos
     row_output_pairs = [(row, output_dir) for row in rows] # porque executor.map solo acepta un argumento por iteración
@@ -81,17 +84,17 @@ def main(output_dir: str, input_dir: str):
         executor.close()
         executor.join()
 
-    duration = time.perf_counter() - start_time
+    #duration = time.perf_counter() - start_time
 
     # Guardar resumen al final
-    with open(log_file, "a") as f:
-        f.write(f"\nResumen:\nDescargados correctamente: {success_count.value}\nFallidos: {fail_count.value}\n")
-        f.write(f"Tiempo total: {duration:.2f} segundos\n")
+    #with open(log_file, "a") as f:
+     #   f.write(f"\nResumen:\nDescargados correctamente: {success_count.value}\nFallidos: {fail_count.value}\n")
+      #  f.write(f"Tiempo total: {duration:.2f} segundos\n")
 
-    print(f"Descarga completa en {duration:.2f} segundos")
-    print(f"Pokémon descargados correctamente: {success_count.value}")
-    print(f"Pokémon fallidos: {fail_count.value}")
-    print(f"Log de depuración guardado en {log_file}")
+    #print(f"Descarga completa en {timed:.2f} segundos")
+    #print(f"Pokémon descargados correctamente: {success_count.value}")
+    #print(f"Pokémon fallidos: {fail_count.value}")
+    #print(f"Log de depuración guardado en {log_file}")
 
 
 
